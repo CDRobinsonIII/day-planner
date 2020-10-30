8bb90f0703ecb1689859e03f13412a107d5794f2
Charles Robinson <the.real.robinsons@gmail.com>
2020-10-30 09:49:37 -0700

    Created function to retrieve stored tasks from the local storage.

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,25 +1,52 @@
 // Use the moment object along with the format method to display the current day on the index.HTML page.
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
+
+// Create a function to retreive any stored tasks from the local storage. 
+// The function is called one at a time for each time slot via the createTimeBlocks function.
+function retrieveStoredTask (timeBlockTask) {
+
+    // Declare var to local storage item.
+    var timeBlockData = localStorage.getItem(timeBlockTask);
+
+    // See if there is data in the local storage for the timeBlockTask.
+    // If there is data, then display it in the text area for that time block.
+    if (timeBlockData) {
+
+        // Grab the task id (with a template literal) which is attached to the <textarea>.
+        // After grabbing the task id set the value .text() to the task in the local storage.
+        $(`#task${timeBlockTask}`).text(timeBlockData)
+    }  
+
+}
+
 // Create function to display 9 different time block on the day planner. This will be done by mainpulating the DOM of the index.HTML page. 
-
-
 function createTimeBlocks () {
 
     // Create a for Loop to run through the 9 different time blocks for the day planner. Start with 9AM (09), end with 5PM (17).
-    // For the iterator use tb for time block.
-    for (i = 0; i <9; i++) {
+    for (i = 9; i <18; i++) {
 
         // Create a row (aka time block) to contain the three columns of data for the time block. There will be 9 different rows. 
-        // Each row needs to have an id stating the row they are, based off of the iterator tb.
+        // Each row needs to have an id stating the row they are, based off of the iterator i starting at 9 for (9am).
         var timeBlockRow = $('<div>').addClass('row time-block');
         row = $(timeBlockRow); 
         
         // Create the 3 different columns that will be in each row. 
         // One column for the time for the time block, one column for the text area containing the task, and another column for the save button. 
+
+        // The first column is the time column.
+        // The data-time attribute is created using the moment.js time method and hour AMPM format (hA). 
+        // This attribute will be used to create the time to be display in the time block column.
         columnTimeBlock = $('<div>').addClass('col-md-1 p-0 hour').text(moment('9:00 AM', 'hh:mm A').add(i, 'hours').format('hA'));
         columnTimeBlock.attr('data-time', moment('9:00 AM', 'hh:mm A').add(i, 'hours').format('hA'));;
-        columnTaskBlock = $('<textarea>').addClass('col-md-9 description p-0 past');
+
+        // The second column is the text area to hold the task for that hour.
+        // The task id created will be use to display the task text from local storage.
+
+        columnTaskBlock = $('<textarea>').addClass('col-md-9 description p-0 past').attr('id', `task${i}`);
+        // console.log("The task id attribute is: " + columnTaskBlock.val());
+
+        // The third column is the save button to save the task to local storage. 
         columnSaveBlock = $('<button>').addClass('saveBtn col-md-1 p-0').html('<i class = "fas fa-save"></i>');
 
         // Append the three columns to the row in the correct order. 
@@ -29,11 +56,22 @@ function createTimeBlocks () {
 
         // Append the row to the container 
         $('.container').append(row);
+
+        retrieveStoredTask (i);
         
     }
 }
 
 createTimeBlocks ();
+
+// Create an addEventListener for when user hits the save button for the tasks.
+
+
+
+
+
+
+
 
 
 
